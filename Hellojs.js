f8a8351ee0c6648bb4f6cb8ef715b5065decc388
sacocho <d.sako3456@gmail.com>
2021-01-22 15:29:05 +0900

    Update Hellojs.js

--- Hellojs.js
+++ Hellojs.js
@@ -1,12 +1,6 @@
-HTML
-<!DOCTYPE html>
-<html>
-<head>
-<title>サンプル</title>
-</head>
-<body>
-<script>
-document.write("Hello world!!");
-</script>
-</body>
-</html>
+var app = new Vue({
+  el: '#app',
+  data: {
+    message: 'こんにちは、ビュー(Vue)！'
+  }
+})
